71065482917bee1d6fd92e48a9f4f2cc0000972b
jackson meyer <jackson.meyer7@gmail.com>
2017-05-09 15:17:28 -0700

    minor space change

--- app/src/main/java/jacksonmeyer/com/earthquakemadness/Constants.java
+++ app/src/main/java/jacksonmeyer/com/earthquakemadness/Constants.java
@@ -5,7 +5,6 @@ package jacksonmeyer.com.earthquakemadness;
  */
 
 //The only constant in this project is the earthquake_url API endpoint
-
 public class Constants {
     public  static final String API_EARTHQUAKE_URL= "http://api.geonames.org/earthquakesJSON?formatted=true&north=44.1&south=-9.9&east=-22.4&west=55.2&username=mkoppelman";
 }
